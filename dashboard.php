44f17a568d50b9e98a5f74827ce7401dd8cf01fd
Cephere <matthewcenteno50@gmail.com>
2025-02-20 20:05:41 +0800

    draft-1

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang = "en" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Procuratio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="nav">
+        <img class="logo" src="img\logo.png" alt="">
+        <h2>Procuratio</h2>
+    </div>
+
+    <div class="container">
+        <div class="hero-container"></div>
+    </div>
+</body>
+
---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang = "en" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Procuratio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<script>
+    function dashboardpageFunction(){
+            window.location.href="dashboard.php";
+        }
+</script>
+
+<body>
+    <div class="nav">
+            <img class="logo" src="img\logo.png" alt="">
+            <h2>Procuratio</h2>
+    </div>
+    
+    <div class="container">
+        <form action="dashboard.php">
+            <input type="text" name="" id="" placeholder="Username">
+            <br>
+            <input type="password" name="" id="" placeholder="Password">
+            <br>
+            <button>Login</button>
+        </form>
+    </div>
+</body>
+
