32bb2e016b891d35691b5e27ab908d96e858879f
BillyNoms <billynombrado@gmail.com>
2025-03-08 09:32:37 +0800

    database table

--- application.php
+++ application.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang = "en" xmlns="http://www.w3.org/1999/xhtml">
-
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -45,7 +44,48 @@
             </div>
         </div>
 
-        <div class="application">
+        <div class="application-table">
+                <table>
+            <tr>
+                    <th>Name</th>
+                    <th>Resume</th>
+                    <th>Application Date</th>
+                    <th>STATUS</th>
+                </tr>
+                    <?php
+                    $conn = mysqli_connect("localhost", "root", "root", "softeng_db");
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+
+                    $sql = "SELECT
+                                U.NAME AS 'Name',
+                                A.resume_link AS 'Resume',
+                                A.applied_at AS 'Application date',
+                                A.STATUS AS 'STATUS'
+                            FROM
+                                applicants AS A
+                                INNER JOIN users AS U ON A.applicant_id = U.user_id";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr>
+                                    <td>" . $row["Name"] . "</td>
+                                    <td>" . $row["Resume"] . "</td>
+                                    <td>" . $row["Application date"] . "</td>
+                                    <td>" . $row["STATUS"] . "</td>
+                                </tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='5'>No results found</td></tr>";
+                    }
+                    $conn->close();
+                ?>
+
+            </table>
+
+
         </div>
     </div>
 </body>
